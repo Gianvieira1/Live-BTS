7ca4f7cad78b1d2bf185b7053905443d6a237011
Gianvieira1 <87399498+Gianvieira1@users.noreply.github.com>
2025-06-05 13:50:04 -0300

    ATT

--- api/notify.js
+++ api/notify.js
@@ -8,8 +8,8 @@ const serviceAccount = {
   "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\\\n/g, '\\n'),
   "client_email": process.env.FIREBASE_PRIVATE_EMAIL,
   "client_id": process.env.FIREBASE_PRIVATE_CLIENTID,
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_uri": process.env.FIREBASE_PRIVATE_AUTH,
+  "token_uri": process.env.FIREBASE_PRIVATE_TOKEN,
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
   "client_x509_cert_url":  process.env.FIREBASE_PRIVATE_X509,
   "universe_domain": "googleapis.com"
